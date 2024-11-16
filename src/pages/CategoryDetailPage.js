ff447d5f2f8d4e4dcfc7fb1c86ae94c49e18477c
RazanAlmohammadi <razanalmohammadi84@gmail.com>
2024-11-16 03:42:36 +0300

    update

--- src/pages/CategoryDetailPage.js
+++ src/pages/CategoryDetailPage.js
@@ -3,7 +3,7 @@ import CategoryDetail from "../components/CategoryDetails/CategoryDetails"
 export default function CategoryDetailPage() {
   return (
       <div>
-        <h1>CategoryDetailPage</h1>
+      
         <CategoryDetail/>
        
         </div>
